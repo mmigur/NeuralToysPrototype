7df9eb6aa7321d52d2bf43cb480fb2df1a40df2c
mmigur <mmigur@bk.ru>
2023-11-19 12:30:04 +0600

    <add>: added story generate function

--- src/config.py
+++ src/config.py
@@ -3,9 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Дополнение к промтам chat-gpt
 KIDS_MODE = os.environ.get("KIDS_MODE")
+ADVICE_TEMPLATE = os.environ.get("ADVICE_TEMPLATE")
 
+# Настройки запроса
 GPT_MODEL_NAME = os.environ.get("GPT_MODEL_NAME")
 STREAM = bool(os.environ.get("STREAM"))
 
-ADVICE_TEMPLATE = os.environ.get("ADVICE_TEMPLATE")
+# Переменные для генерации сказки
+STORY_PROMT_PART_1 = os.environ.get("STORY_PROMT_PART_1")
+STORY_PROMT_PART_2 = os.environ.get("STORY_PROMT_PART_2")
+STORY_PROMT_PART_3 = os.environ.get("STORY_PROMT_PART_3")
+STORY_PROMT_PART_4 = os.environ.get("STORY_PROMT_PART_4")
--- src/neural_toys_model.py
+++ src/neural_toys_model.py
@@ -12,6 +12,11 @@ class NeuralToysModel:
         self.name = name
 
     def answer_to_questions(self, questions: str) -> str:
+        """
+        Функция для ответа на вопрос ребенка простым языком.
+        Принимает: вопрос ребенка.
+        Возвращает: ответ, который ребенок может понять.
+        """
         content_question = questions + \
                            config.KIDS_MODE + \
                            self.age + \
@@ -26,10 +31,47 @@ class NeuralToysModel:
         return "".join(response)
     
     def get_advide(self) -> str:
+        """
+        Функция, которая дает совет ребенку, помогающим ему существовать.
+        Возвращает: совет для ребенка.
+        """
         content_question = config.ADVICE_TEMPLATE + \
                            config.KIDS_MODE + \
                            self.age + \
                            self.name
+        
+        response = g4f.ChatCompletion.create(
+            model=config.GPT_MODEL_NAME,
+            messages=[{"role": "user", "content": content_question}],
+            stream=config.STREAM
+        )
+
+        return "".join(response)
+    
+
+    def generate_story(
+            main_characters: str, 
+            mission_of_main_characters: str,
+            universe: str,
+            additional_characters: str,
+            user_additions: str
+        ) -> str:
+        """
+        Функция, генерирующая сказку для ребенка по запросам его родителя.
+        Принимает: описание главного героя, задачу которую он будет решать,
+                   описание мира в котором будет происходит сказка,
+                   дополнительные персонажи участвующие в сказке,
+                   и пожелания пользователя.
+        Возвращает: сказу с параметрами пользователями.
+        """
+        content_question = config.STORY_PROMT_PART_1 + \
+                           main_characters + \
+                           config.STORY_PROMT_PART_2 + \
+                           mission_of_main_characters + \
+                           config.STORY_PROMT_PART_3 + \
+                           universe + config.STORY_PROMT_PART_4 + \
+                           additional_characters + \
+                           user_additions
         
         response = g4f.ChatCompletion.create(
             model=config.GPT_MODEL_NAME,
